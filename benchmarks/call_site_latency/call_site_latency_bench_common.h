b7dbfc3c6a23672add52c724724b13615ac0a211
Odysseas.Georgoudis <odygrd@gmail.com>
2020-02-24 01:20:49 +0000

    update results

--- benchmarks/call_site_latency/call_site_latency_bench_common.h
+++ benchmarks/call_site_latency/call_site_latency_bench_common.h
@@ -10,6 +10,6 @@
 #define ITERATIONS                                                                                 \
   std::size_t { 100000 }
 #define MIN_WAIT_DURATION                                                                          \
-  std::chrono::microseconds { 2 }
+  std::chrono::microseconds { 1 }
 #define MAX_WAIT_DURATION                                                                          \
   std::chrono::microseconds { 100 }
