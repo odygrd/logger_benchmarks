27c1a3ab93a3727181ca6b395267e0118e182bf0
odygrd <odygrd@gmail.com>
2024-08-21 23:55:02 +0100

    zzz

--- run_all.py
+++ run_all.py
@@ -1,6 +1,6 @@
 import subprocess
 
-root_dir = "/home/odygrd/CLionProjects/logger_benchmarks/cmake-build-release/benchmarks/call_site_latency/"
+root_dir = "/home/odygrd/git/logger_benchmarks/cmake-build-release/benchmarks/call_site_latency/"
 
 benchmark_quill_unbounded_call_site_latency_int_int_double = root_dir + "benchmark_quill_unbounded_call_site_latency_int_int_double"
 benchmark_quill_bounded_call_site_latency_int_int_double = root_dir + "benchmark_quill_bounded_call_site_latency_int_int_double"
@@ -11,6 +11,7 @@ benchmark_fmtlog_int_int_double = root_dir + "benchmark_fmtlog_call_site_latency
 benchmark_platformlab_int_int_double = root_dir + "benchmark_platformlab_call_site_latency_int_int_double"
 benchmark_reckless_int_int_double = root_dir + "benchmark_reckless_call_site_latency_int_int_double"
 benchmark_spdlog_int_int_double = root_dir + "benchmark_spdlog_call_site_latency_int_int_double"
+benchmark_xtr_int_int_double = root_dir + "benchmark_xtr_call_site_latency_int_int_double"
 
 benchmark_quill_unbounded_call_site_latency_int_int_largestr = root_dir + "benchmark_quill_unbounded_call_site_latency_int_int_largestr"
 benchmark_quill_bounded_call_site_latency_int_int_largestr = root_dir + "benchmark_quill_bounded_call_site_latency_int_int_largestr"
@@ -21,16 +22,19 @@ benchmark_fmtlog_int_int_largestr = root_dir + "benchmark_fmtlog_call_site_laten
 benchmark_platformlab_int_int_largestr = root_dir + "benchmark_platformlab_call_site_latency_int_int_largestr"
 benchmark_reckless_int_int_largestr = root_dir + "benchmark_reckless_call_site_latency_int_int_largestr"
 benchmark_spdlog_int_int_largestr = root_dir + "benchmark_spdlog_call_site_latency_int_int_largestr"
+benchmark_xtr_int_int_largestr = root_dir + "benchmark_xtr_call_site_latency_int_int_largestr"
 
 benchmark_quill_unbounded_call_site_latency_vector_largestr = root_dir + "benchmark_quill_unbounded_call_site_latency_vector_largestr"
 benchmark_quill_bounded_call_site_latency_vector_largestr = root_dir + "benchmark_quill_bounded_call_site_latency_vector_largestr"
 benchmark_fmtlog_call_site_latency_int_vector_largestr = root_dir + "benchmark_fmtlog_call_site_latency_int_vector_largestr"
 benchmark_ms_binlog_call_site_latency_int_vector_largestr = root_dir + "benchmark_ms_binlog_call_site_latency_int_vector_largestr"
 benchmark_spdlog_call_site_latency_int_vector_largestr = root_dir + "benchmark_spdlog_call_site_latency_int_vector_largestr"
+benchmark_xtr_call_site_latency_int_vector_largestr = root_dir + "benchmark_spdlog_call_site_latency_int_vector_largestr"
 
 benchmarks = [benchmark_quill_unbounded_call_site_latency_int_int_double,
               benchmark_quill_bounded_call_site_latency_int_int_double,
               benchmark_fmtlog_int_int_double,
+              benchmark_xtr_int_int_double,
               benchmark_platformlab_int_int_double,
               benchmark_ms_binlog_int_int_double,
               benchmark_spdlog_int_int_double,
@@ -40,6 +44,7 @@ benchmarks = [benchmark_quill_unbounded_call_site_latency_int_int_double,
               benchmark_quill_unbounded_call_site_latency_int_int_largestr,
               benchmark_quill_bounded_call_site_latency_int_int_largestr,
               benchmark_fmtlog_int_int_largestr,
+              benchmark_xtr_int_int_largestr,
               benchmark_platformlab_int_int_largestr,
               benchmark_ms_binlog_int_int_largestr,
               benchmark_spdlog_int_int_largestr,
@@ -49,6 +54,7 @@ benchmarks = [benchmark_quill_unbounded_call_site_latency_int_int_double,
               benchmark_quill_unbounded_call_site_latency_vector_largestr,
               benchmark_quill_bounded_call_site_latency_vector_largestr,
               benchmark_fmtlog_call_site_latency_int_vector_largestr,
+              benchmark_xtr_call_site_latency_int_vector_largestr,
               benchmark_ms_binlog_call_site_latency_int_vector_largestr,
               benchmark_spdlog_call_site_latency_int_vector_largestr]
 
